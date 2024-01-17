c372188f28ad7182edb6a6f805260d9d864cef77
hyrious <hyrious@outlook.com>
2024-01-17 18:41:47 +0800

    export version

--- src/client.d.ts
+++ src/client.d.ts
@@ -2,3 +2,5 @@ declare module "*?inline" {
   const contents: string
   export default contents
 }
+
+declare const __VERSION__: string
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export * from "./presentation"
 export * from "./app-presentation"
 export { NetlessAppPresentation as default } from "./app-presentation"
+
+export const version = __VERSION__
